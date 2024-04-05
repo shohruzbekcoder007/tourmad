031ebfc63ed743713aa7d0b4517bc2fdff8dd30e
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-04-05 21:27:17 +0300

    payed to figma1

--- src/theme/mui/light.ts
+++ src/theme/mui/light.ts
@@ -4,13 +4,23 @@ export const lightThemeOptions: ThemeOptions = {
   palette: {
     mode: 'light',
     primary: {
-      main: '#0f0'
+      main: '#8DD3BB',
+      contrastText: '#112211',
     },
   },
   typography: {
     fontFamily:
       '"Montserrat"',
-  }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontFamily: '"Montserrat"',
+        }
+      }
+    }
+  },
 };
 
 export const lightTheme = createTheme(lightThemeOptions);
