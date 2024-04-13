d72e5713e35146092a9f2f667cff2b86bcfb978c
ibrakhimov-dev <dev.ibrakhimov@gmail.com>
2024-04-13 21:20:35 +0500

    start

--- src/components/TripPlan/index.tsx
+++ src/components/TripPlan/index.tsx
@@ -1,7 +1,20 @@
+import { Box, Grid } from "@mui/material";
 import React from "react";
+import { Container, WelcomeMainText } from "../../global_styles/styles";
 const TripPlan: React.FC = () => {
     return (
-        <div>dgdfg</div>
+        <Container>
+            <Box>
+                <Grid container>
+                    <Grid item xl={8} md={8} sm={6} xs={6}>
+                        <WelcomeMainText fontSize={"32px"} part>Plan your perfect trip</WelcomeMainText>
+                    </Grid>
+                    <Grid item xl={4} md={4} sm={6} xs={6}>
+
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
