5afb827b9ac9484e2485816c12f25ab85ea3f74c
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-04-13 19:19:59 +0300

    continer margin 0 auto

---
+++ src/components/TicketTable/index.tsx
@@ -0,0 +1,10 @@
+import { Paper } from '@mui/material'
+import React from 'react'
+
+const TicketTable: React.FC = () => {
+  return (
+    <Paper>TicketTable</Paper>
+  )
+}
+
+export default TicketTable
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Welcome } from './Welcome'
 export { default as Header } from './Header'
 export { default as TripPlan } from './TripPlan'
+export { default as TicketTable } from './TicketTable'
