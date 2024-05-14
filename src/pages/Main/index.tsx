eaf399512669c94a1d4b8c8ad2a4e7b1eef3225e
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-05-14 18:13:18 +0300

    login started

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Wrapper } from '../../global_styles/styles';
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 
 const Main: React.FC = () => {
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    // navigate("public")
-  }, [])
-
   return (
     <Wrapper height="100vh">
       <Outlet/>
