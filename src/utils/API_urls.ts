bf5bd4547a41c261a7be5bdaa9c3e61b8dad24a6
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-07-07 22:29:30 +0300

    save login password

--- src/utils/API_urls.ts
+++ src/utils/API_urls.ts
@@ -1,7 +1,7 @@
 export const host: string = (process.env.REACT_APP_API_URL as string)
 
 export const login: string = 'accounts/login/'
-export const token_refresh: string = 'accounts/TokenRefresh/'
+export const token_refresh: string = 'accounts/TokenRefresh'
 export const me: string = 'accounts/me/'
 
 export const trip_hotel: string = "trip/trip-hotel"
