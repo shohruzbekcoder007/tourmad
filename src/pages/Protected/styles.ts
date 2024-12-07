0f3daf9b9e3596439c3873fff128ab941fc44869
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-12-07 14:48:35 +0300

    nodirning brenchiga ishlangan

--- src/pages/Protected/styles.ts
+++ src/pages/Protected/styles.ts
@@ -5,6 +5,6 @@ export const HeaderWrapper = styled(Box)`
     box-shadow: 0px 4px 16px 0px rgba(17, 34, 17, 0.05);
     position: sticky;
     top: 0;
-    z-index: 10;
+    z-index: 2000;
     background-color: ${props => props.theme.main_bg};
 `
